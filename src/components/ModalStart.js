e4b1cfe08bc3927f4c7720953ed4e823f2b47edb
Merenalis <70169071+Merenalis@users.noreply.github.com>
2021-08-10 12:39:00 +0300

    first

---
+++ src/components/ModalStart.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+
+export default class ModalStart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          display: 'block'
+        };
+      }
+    
+      hideWindow(e) {
+        this.setState({ display: 'none' });
+      }
+ 
+    
+    render() {
+        const style  = { display: this.state.display }
+        return (        
+
+
+<div id="modal-start" style={style}>
+    <div id="overlay-start">
+        <div id="modal-window-start">
+            <div id="content-start">
+                Чтобы начать новую игру, нажмите Старт.<br/>
+                <p> Пусть победит сильнейший!</p>
+            </div>
+            <div id="btn-start_game" onClick={e=>this.hideWindow()}>
+                Старт
+            </div>
+        </div>
+    </div>
+</div>
+
+        
+        );
+    }
+}
